6c84259eadc636239d00fe31c0d8771b90830ebe
ieba2 <jeuxi.g06@gmail.com>
2024-02-07 17:52:34 +0100

    modified:   js/transition.js

--- js/transition.js
+++ js/transition.js
@@ -3,7 +3,7 @@ const header = document.getElementById('header')
 // J'en fait une fonction pour pouvoir l'appeler au chargement de la page car
 // le scoll n'est pas forcément en haut au chargement.
 function onWindowScroll(event) {
-	if (window.pageYOffset < 46) {
+	if (window.scrollY < 20) {
   	header.classList.remove('scrolled')
   } else {
   	header.classList.add('scrolled')
